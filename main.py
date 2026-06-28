c6e473cdc0aa291c6fd016b4c9ac7b08d89b22d2
Will Taufoou <will.taufoou45@gmail.com>
2026-06-27 20:20:35 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+print("Athlete Intelligence Platform")
+
+athlete_name = "Will"
+sport = "Football"
+ten_yard_time = 1.72
+
+print()
+print("Athlete Report")
+print("----------------")
+print("Name:", athlete_name)
+print("Sport:", sport)
+print("10 Yard Sprint:", ten_yard_time, "seconds")
